de302a20fb1eb09cb83864ce1da99373b12f762a
Brian Lagranda <brianlagranda@gmail.com>
2025-05-06 14:35:22 -0300

    feat: implement protected routes to restrict access to authenticated users

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router";
 
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import { ProtectedRoute } from "../app/ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
