a872c3bdf2db75dc24686b60dbb2c812b6b34254
Brian Lagranda <brianlagranda@gmail.com>
2025-05-09 17:57:03 -0300

    test: add setupTests and unit tests for Button and Input components

---
+++ src/shared/__tests__/Button.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi, describe, it, expect } from "vitest";
+import Button from "../ui/Button";
+
+describe("Button", () => {
+  it("should render login button with the correct text passed through children", () => {
+    render(<Button>Login</Button>);
+
+    const button = screen.getByRole("button", { name: /login/i });
+    expect(button).not.toBeNull();
+  });
+
+  it("should handle onClick event", () => {
+    const handleClick = vi.fn();
+
+    render(<Button onClick={handleClick}>Login</Button>);
+
+    const button = screen.getByRole("button", { name: /login/i });
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("should not be clickable when disabled", () => {
+    const handleClick = vi.fn();
+
+    render(
+      <Button onClick={handleClick} disabled={true}>
+        Login
+      </Button>,
+    );
+
+    const button = screen.getByRole("button", { name: /login/i });
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(0);
+  });
+
+  it("should have type 'submit'", () => {
+    render(<Button type="submit">Login</Button>);
+
+    const button = screen.getByRole("button", { name: /login/i });
+
+    expect(button.getAttribute("type")).toBe("submit");
+  });
+});
---
+++ src/shared/__tests__/Input.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi, describe, it, expect, beforeEach } from "vitest";
+import Input from "../ui/Input";
+
+describe("Email Input", () => {
+  let handleChange: ReturnType<typeof vi.fn>;
+
+  beforeEach(() => {
+    handleChange = vi.fn();
+    render(
+      <Input
+        label="email"
+        name="email"
+        type="email"
+        value="test@finapp.com"
+        onChange={handleChange}
+      ></Input>,
+    );
+  });
+
+  it("should render an email input with correct label", () => {
+    const input = screen.getByLabelText("email");
+    expect(input).not.toBeNull();
+  });
+
+  it("should have type email", () => {
+    const input = screen.getByLabelText("email");
+    expect(input.getAttribute("type")).toBe("email");
+  });
+
+  it("should call onChange for each character typed", async () => {
+    const input = screen.getByLabelText("email");
+
+    await userEvent.type(input, "testing");
+
+    expect(handleChange).toHaveBeenCalledTimes(7);
+  });
+});
