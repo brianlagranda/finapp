96270872b53d4238175b6fc3d129003d6e36e5ea
Brian Lagranda <brianlagranda@gmail.com>
2025-05-30 13:35:12 -0300

    feat: add confirmation modal before deleting a transaction

--- src/features/transactions/ui/TransactionList.tsx
+++ src/features/transactions/ui/TransactionList.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import { sortTransactionsByDateDesc } from "../lib/sortTransactions";
 import useTransactionStore from "../model/useTransactionStore";
 import { TransactionItem } from "./TransactionItem";
 import toast from "react-hot-toast";
+import Modal from "../../../shared/ui/Modal";
+import Button from "../../../shared/ui/Button";
+import { Transaction } from "../model/types";
 
 const TransactionList = () => {
   const transactions = useTransactionStore((state) => state.transactions);
   const deleteTransaction = useTransactionStore(
     (state) => state.deleteTransaction,
   );
+  const [transactionToDelete, setTransactionToDelete] =
+    useState<Transaction | null>(null);
 
-  const handleDelete = (id: string) => {
-    deleteTransaction(id);
-    toast.success("Transaction succesfully deleted");
+  const handleDeleteConfirmed = () => {
+    if (transactionToDelete) {
+      deleteTransaction(transactionToDelete.id);
+      toast.success("Transaction succesfully deleted");
+      setTransactionToDelete(null);
+    }
   };
 
   const transactionsOrderedByDate = sortTransactionsByDateDesc(transactions);
@@ -26,26 +35,53 @@ const TransactionList = () => {
   }
 
   return (
-    <table className="w-full text-left text-sm" aria-label="Transactions table">
-      <thead>
-        <tr className="border-b">
-          <th className="px-4 py-2">Cat.</th>
-          <th className="px-4 py-2">Date</th>
-          <th className="px-4 py-2">Description</th>
-          <th className="px-4 py-2">Amount</th>
-          <th className="px-4 py-2" aria-hidden="true"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactionsOrderedByDate.map((transaction) => (
-          <TransactionItem
-            key={transaction.id}
-            transaction={transaction}
-            onDelete={handleDelete}
-          />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table
+        className="w-full text-left text-sm"
+        aria-label="Transactions table"
+      >
+        <thead>
+          <tr className="border-b">
+            <th className="px-4 py-2">Cat.</th>
+            <th className="px-4 py-2">Date</th>
+            <th className="px-4 py-2">Description</th>
+            <th className="px-4 py-2">Amount</th>
+            <th className="px-4 py-2" aria-hidden="true"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactionsOrderedByDate.map((transaction) => (
+            <TransactionItem
+              key={transaction.id}
+              transaction={transaction}
+              onDelete={() => setTransactionToDelete(transaction)}
+            />
+          ))}
+        </tbody>
+      </table>
+
+      <Modal
+        isOpen={!!transactionToDelete}
+        onClose={() => setTransactionToDelete(null)}
+      >
+        <div>
+          <p className="text-lg font-semibold">
+            Are you sure you want to delete{" "}
+            {`"${transactionToDelete?.title} - ${transactionToDelete?.description ? transactionToDelete.description : ""}"`}{" "}
+            transaction?
+          </p>
+          <div className="mt-4 flex justify-end gap-2">
+            <Button onClick={() => setTransactionToDelete(null)}>Cancel</Button>
+            <Button
+              className="bg-red-500 text-white hover:bg-red-700"
+              onClick={handleDeleteConfirmed}
+            >
+              Delete
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
