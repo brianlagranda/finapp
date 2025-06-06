1d29b48a1058bc0426069fc70ca710b3f1234005
Brian Lagranda <brianlagranda@gmail.com>
2025-06-06 12:11:55 -0300

    feat: add desktop version of AddTransactionButton with text and hide mobile icon on desktop

--- src/features/dashboard/ui/AddTransactionButton.tsx
+++ src/features/dashboard/ui/AddTransactionButton.tsx
@@ -7,13 +7,24 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const AddTransactionButton = ({ onClick, ...props }: ButtonProps) => {
   return (
-    <button
-      aria-label="Add Transaction"
-      onClick={onClick}
-      className="btn-icon fixed right-4 bottom-4 z-50 overflow-hidden rounded-full active:scale-95"
-      {...props}
-    >
-      <Plus size={44} strokeWidth={1.5} className="" />
-    </button>
+    <>
+      <button
+        aria-label="Add Transaction"
+        onClick={onClick}
+        className="btn-icon fixed right-4 bottom-4 z-50 overflow-hidden rounded-full active:scale-95 md:hidden"
+        {...props}
+      >
+        <Plus size={44} strokeWidth={1.5} className="" />
+      </button>
+
+      <button
+        aria-label="Add Transaction"
+        onClick={onClick}
+        className="btn-primary hidden cursor-pointer overflow-hidden rounded-full active:scale-95 md:mx-auto md:my-4 md:block"
+        {...props}
+      >
+        + Add Transaction
+      </button>
+    </>
   );
 };
