4fb01ee97fc9fea3a4207bf9874564b966f1eb02
Brian Lagranda <briandvlpr@gmail.com>
2025-06-23 13:01:51 -0300

    feat(dashboard): add header with dropdown menu and reorganize Dashboard structure

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Balance } from "../../features/dashboard/ui/Balance";
+import TransactionList from "../../features/transactions/ui/TransactionList";
+
+import TransactionForm from "../../features/transactions/ui/TransactionForm";
+import Modal from "../../shared/ui/Modal";
+import { AddTransactionButton } from "../../features/dashboard/ui/AddTransactionButton";
+import { Header } from "../../widgets/header/ui/Header";
+
+function Dashboard() {
+  const [renderTransactionForm, setRenderTransactionForm] =
+    useState<boolean>(false);
+
+  return (
+    <>
+      <Header />
+      <h2 className="sr-only">Dashboard</h2>
+      <Balance />
+      <section>
+        <h2 className="mb-4 text-xl font-semibold">Recent Transactions</h2>
+        {<TransactionList />}
+      </section>
+      <Modal
+        isOpen={renderTransactionForm}
+        onClose={() => setRenderTransactionForm(false)}
+      >
+        <TransactionForm />
+      </Modal>
+      {!renderTransactionForm && (
+        <AddTransactionButton onClick={() => setRenderTransactionForm(true)} />
+      )}
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,10 @@
+import { DropdownMenu } from "../../../shared/ui/DropdownMenu";
+
+export const Header = () => {
+  return (
+    <header className="mx-auto flex max-w-7xl items-center justify-between rounded-b-lg bg-white/80 p-4 shadow-sm backdrop-blur">
+      <h1 className="text-primary text-2xl font-bold">Finapp</h1>
+      <DropdownMenu />
+    </header>
+  );
+};
