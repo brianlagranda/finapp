65b7770677d7184509a7ed13f9f7fd6e9eedb851
Brian Lagranda <briandvlpr@gmail.com>
2025-07-02 15:21:53 -0300

    chore: remove obsolete sortTransactions utility

--- src/features/transactions/lib/sortTransactions.ts
+++
@@ -1,6 +0,0 @@
-import { Transaction } from "../model/types";
-
-export const sortTransactionsByDateDesc = (transactions: Transaction[]) =>
-  [...transactions].sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
-  );
