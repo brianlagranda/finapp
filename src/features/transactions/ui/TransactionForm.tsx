9bb6607af57610c8d7e0138fe655685683ed8aad
Brian Lagranda <briandvlpr@gmail.com>
2025-07-10 13:10:24 -0300

    feat(form): support edit mode in TransactionForm with dynamic initial values, validation and UI feedback

--- src/features/transactions/ui/TransactionForm.tsx
+++ src/features/transactions/ui/TransactionForm.tsx
@@ -4,9 +4,16 @@ import * as Yup from "yup";
 import Button from "../../../shared/ui/Button";
 import FormField from "../../../shared/ui/FormField";
 import useTransactionStore from "../model/useTransactionStore";
-import { TransactionType } from "../model/types";
+import { Transaction } from "../model/types";
 import { categories } from "../../../shared/constants/categories";
 import { formatTransactionDate } from "../lib/formatTransactionDate";
+
+type TransactionFormProps = {
+  mode: "create" | "edit";
+  initialData?: Transaction;
+};
+
+type TransactionFormValues = Omit<Transaction, "id">;
 
 const validCategoryValues = categories.map((category) => category.value);
 
@@ -26,46 +33,67 @@ const TransactionSchema = Yup.object({
   description: Yup.string(),
 });
 
-const TransactionForm = () => {
+const TransactionForm = ({ mode, initialData }: TransactionFormProps) => {
   const addTransaction = useTransactionStore((state) => state.addTransaction);
+  const updateTransaction = useTransactionStore(
+    (state) => state.updateTransaction,
+  );
+
+  const isEditing = mode === "edit";
+
+  const initialValues: TransactionFormValues =
+    isEditing && initialData
+      ? {
+          title: initialData.title ?? "",
+          type: initialData.type ?? "expense",
+          amount: initialData.amount ?? 0,
+          date: initialData.date ?? new Date().toISOString().split("T")[0],
+          category: initialData.category ?? "food",
+          description: initialData.description ?? "",
+        }
+      : {
+          title: "",
+          type: "income",
+          amount: 0,
+          date: new Date().toISOString().split("T")[0],
+          category: "food",
+          description: "",
+        };
+
   return (
-    <Formik
-      initialValues={{
-        title: "",
-        type: "income",
-        amount: null,
-        date: new Date().toISOString().split("T")[0],
-        category: "food",
-        description: "",
-      }}
+    <Formik<TransactionFormValues>
+      initialValues={initialValues}
       validationSchema={TransactionSchema}
       onSubmit={(values, { setSubmitting, resetForm }) => {
-        setTimeout(() => {
-          const formattedDate = formatTransactionDate(values.date);
+        const formattedDate = formatTransactionDate(values.date);
 
-          const newTransaction = {
+        if (isEditing && initialData) {
+          updateTransaction({
+            ...initialData,
             ...values,
-            id: crypto.randomUUID(),
-            type: values.type as TransactionType,
-            title: values.title.trim(),
-            description: values.description.trim(),
-            amount: Number(values.amount),
             date: formattedDate,
-          };
+          });
+        } else {
+          addTransaction({
+            ...values,
+            title: values.title.trim(),
+            description: values.description?.trim(),
+            id: crypto.randomUUID(),
+            date: formattedDate,
+          });
+        }
 
-          addTransaction(newTransaction);
-
-          setSubmitting(false);
-          resetForm();
-        }, 400);
+        setSubmitting(false);
+        resetForm();
       }}
     >
       {({ isSubmitting }) => (
         <Form className="mx-auto flex w-[345px] flex-col justify-evenly gap-4 rounded-lg bg-white/60 p-4 backdrop-blur-md">
+          <h2>{isEditing ? "Edit transaction" : "Create new transaction"}</h2>
           <FormField name="title" label="Title" />
           <FormField
             name="type"
-            label="type"
+            label="Type"
             as="select"
             options={[
               {
@@ -78,7 +106,7 @@ const TransactionForm = () => {
           <FormField name="amount" label="Amount" type="number" />
           <FormField
             name="category"
-            label="category"
+            label="Category"
             as="select"
             options={categories}
           />
@@ -86,7 +114,7 @@ const TransactionForm = () => {
           <FormField name="description" label="Description" />
 
           <Button type="submit" disabled={isSubmitting}>
-            Save
+            {isEditing ? "Save" : "Create"}
           </Button>
         </Form>
       )}
